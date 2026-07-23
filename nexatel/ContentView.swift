ea437269891a6552b742fafe0c405e8ff6f65259
Sabuj Hasan Sarker <sabujhasansarker@gmail.com>
2026-07-24 05:31:31 +0600

    Initial Commit

---
+++ nexatel/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  nexatel
+//
+//  Created by Sabuj on 24/7/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ nexatel/nexatelApp.swift
@@ -0,0 +1,17 @@
+//
+//  nexatelApp.swift
+//  nexatel
+//
+//  Created by Sabuj on 24/7/26.
+//
+
+import SwiftUI
+
+@main
+struct nexatelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
